295562b77152c858fa82387ab0b1435baff6e0d3
YoucanCallmeMonster <149237563+YoucanCallmeMonster@users.noreply.github.com>
2024-01-11 22:25:56 +0900

    [Gold III] Title: 줄 세우기, Time: 1988 ms, Memory: 3984 KB -BaekjoonHub

---
+++ 백준/Gold/2252. 줄 세우기/줄 세우기.cc
@@ -0,0 +1,58 @@
+#include <iostream>
+#include <cstdio>
+#include <vector>
+#include <algorithm>
+#include <cmath>
+#include <stack>
+#include <queue>
+
+using namespace std;
+
+//Globals
+
+int main(int argc, char* argv[]) {
+	int N, M;
+	scanf("%d%d", &N, &M);
+	
+	vector<vector<int>> graph(N+1); // 그래프 인접 리스트
+	int indegree[32001] = {0, }; // 진입차수, N 최대값 32000
+	vector<int> order; // 순서
+	
+	for(int i=0; i<M; i++) {
+		int u, v;
+		scanf("%d%d", &u, &v); // u > v 방향
+		
+		graph[u].push_back(v); // 인접 리스트 추가
+		indegree[v]++; // 진입차수 증가
+	}
+	
+	queue<int> q; // 위상정렬 시 사용할 큐
+	
+	for(int i=1; i<=N; i++) {
+		if(indegree[i]==0) {
+			q.push(i);
+			indegree[i] = -1; // 방문했다는 표시로 진입차수 -1 할당
+		}
+	} // 진입차수가 0인 정점을 큐에 추가
+	
+	while(!q.empty()) {
+		int temp = q.front(); // 탐색할 정점
+		q.pop(); // 큐에서 제거
+		order.push_back(temp); // 순서에 추가
+		
+		for(auto x:graph[temp]) {
+			indegree[x]--;
+		} // 정점 temp와 연결된 간선 제거
+		
+		for(int i=1; i<=N; i++) {
+			if(indegree[i]==0) {
+				q.push(i);
+				indegree[i] = -1; // 방문했다는 표시로 진입차수 -1 할당
+			}
+		} // 진입차수가 0인 정점을 큐에 추가
+	}
+	
+	for(auto x:order) {
+		printf("%d ", x);
+	}
+}
