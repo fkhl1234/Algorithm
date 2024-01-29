b71f270313fd64d98abb272cac7bc873831586cd
YoucanCallmeMonster <149237563+YoucanCallmeMonster@users.noreply.github.com>
2024-01-29 22:19:22 +0900

    [Bronze V] Title: 그대로 출력하기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/11718. 그대로 출력하기/그대로 출력하기.cc
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <cstdio>
+#include <vector>
+#include <algorithm>
+#include <cmath>
+#include <stack>
+#include <queue>
+
+using namespace std;
+
+//Globals
+
+int main(int argc, char* argv[]) {
+	char s[101];
+	
+	while(scanf("%[^\n]\n", s)==1) {
+		printf("%s\n", s);
+	}
+}
