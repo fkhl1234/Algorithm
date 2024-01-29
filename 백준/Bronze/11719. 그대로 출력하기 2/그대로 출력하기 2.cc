76ef859e234055cfa6697e928bcabff6f76af6fa
YoucanCallmeMonster <149237563+YoucanCallmeMonster@users.noreply.github.com>
2024-01-29 22:30:21 +0900

    [Bronze III] Title: 그대로 출력하기 2, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/11719. 그대로 출력하기 2/그대로 출력하기 2.cc
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <cstdio>
+#include <vector>
+#include <algorithm>
+#include <cmath>
+#include <stack>
+#include <queue>
+
+using namespace std;
+
+//Globals
+
+int main(int argc, char* argv[]) {
+	char c;
+	
+	while((c=getchar())&&(c!=EOF)) {
+		printf("%c", c);
+	}
+}
