8a2f0eba68a622d50aa4c89b0c07c430e8c4bdff
YoucanCallmeMonster <149237563+YoucanCallmeMonster@users.noreply.github.com>
2024-01-29 22:35:18 +0900

    [Bronze III] Title: 열 개씩 끊어 출력하기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/11721. 열 개씩 끊어 출력하기/열 개씩 끊어 출력하기.cc
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <cstdio>
+#include <vector>
+#include <algorithm>
+#include <cmath>
+#include <stack>
+#include <queue>
+
+using namespace std;
+
+//Globals
+
+int main(int argc, char* argv[]) {
+	char s[11];
+	
+	while(scanf("%10[^\n]", s)>0) {
+		printf("%s\n", s);
+	}
+}
