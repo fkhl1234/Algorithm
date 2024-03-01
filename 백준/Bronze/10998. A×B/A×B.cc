b1abcb02c1c99ca9983ff60399103dd0474cee1a
YoucanCallmeMonster <149237563+YoucanCallmeMonster@users.noreply.github.com>
2024-03-01 23:46:47 +0900

    [Bronze V] Title: A×B, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10998. A×B/A×B.cc
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <cstdio>
+#include <vector>
+#include <algorithm>
+#include <cmath>
+#include <stack>
+#include <queue>
+
+using namespace std;
+
+//Globals
+
+int main(int argc, char* argv[]) {
+	int N, M;
+	scanf("%d %d", &N, &M);
+	
+	printf("%d", N*M);
+}
