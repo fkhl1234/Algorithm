74674a20bee5666ed1c101b5a13cbec0328f757b
YoucanCallmeMonster <149237563+YoucanCallmeMonster@users.noreply.github.com>
2024-03-02 00:24:00 +0900

    [Bronze V] Title: 1998년생인 내가 태국에서는 2541년생?!, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/18108. 1998년생인 내가 태국에서는 2541년생？！/1998년생인 내가 태국에서는 2541년생？！.cc
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <cstdio>
+#include <vector>
+#include <algorithm>
+#include <cmath>
+#include <stack>
+#include <queue>
+
+using namespace std;
+
+//Globals
+
+int main(int argc, char* argv[]) {
+	int year;
+	scanf("%d", &year);
+	
+	printf("%d\n", year-543);
+}
