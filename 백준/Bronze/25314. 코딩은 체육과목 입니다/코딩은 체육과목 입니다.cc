20f67d45d15f7b6c6a0859214b49f71d2a6595a6
YoucanCallmeMonster <149237563+YoucanCallmeMonster@users.noreply.github.com>
2024-03-02 23:35:52 +0900

    [Bronze V] Title: 코딩은 체육과목 입니다, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/25314. 코딩은 체육과목 입니다/코딩은 체육과목 입니다.cc
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <cstdio>
+#include <vector>
+#include <algorithm>
+#include <cmath>
+#include <stack>
+#include <queue>
+
+using namespace std;
+
+//Globals
+
+int main(int argc, char* argv[]) {
+	int N;
+	scanf("%d", &N);
+	
+	for(int i=0; i<N/4; i++) {
+		printf("long ");
+	}
+	printf("int\n");
+}
