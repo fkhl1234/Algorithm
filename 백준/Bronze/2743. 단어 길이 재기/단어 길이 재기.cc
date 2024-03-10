19df66dda4d09539062132dfb665cc0b51861d1b
YoucanCallmeMonster <149237563+YoucanCallmeMonster@users.noreply.github.com>
2024-03-10 22:40:19 +0900

    [Bronze V] Title: 단어 길이 재기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2743. 단어 길이 재기/단어 길이 재기.cc
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <cstdio>
+#include <vector>
+#include <algorithm>
+#include <cmath>
+#include <stack>
+#include <queue>
+
+using namespace std;
+
+//Globals
+
+int main(int argc, char* argv[]) {
+	int index = 0;
+	char s;
+	while((s = getchar()) != '\n') {
+		index++;
+	}
+	
+	printf("%d\n", index);
+}
