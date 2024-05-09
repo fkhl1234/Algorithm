0e98976395713c46e59e464c15d82e1bf689dd92
fkhl1234 <149237563+fkhl1234@users.noreply.github.com>
2024-05-09 23:15:50 +0900

    [Bronze I] Title: 소인수분해, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/11653. 소인수분해/소인수분해.cc
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <cstdio>
+#include <cmath>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int main()
+{
+    int n;
+    scanf("%d", &n);
+    
+    for(int i=2; i*i<=n; i++) {
+        while(n%i==0) {
+            printf("%d\n", i);
+            n/=i;
+        }
+    }
+    
+    if(n>1) printf("%d\n", n);
+    
+    return 0;
+}
