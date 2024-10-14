52780709cad840e025e7addc54dc3b1f32873caa
fkhl1234 <149237563+fkhl1234@users.noreply.github.com>
2024-10-14 20:23:17 +0900

    [Bronze V] Title: 직사각형, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/27323. 직사각형/직사각형.cc
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <cstdio>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int main() {
+     int x, y, w, h;
+
+     scanf("%d %d", &x, &y);
+     
+     printf("%d\n", x*y);
+
+     return 0;
+}
