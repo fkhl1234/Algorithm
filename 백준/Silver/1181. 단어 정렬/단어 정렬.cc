06a66606b9c64d61a78999a7ad54b614c25e07d3
fkhl1234 <149237563+fkhl1234@users.noreply.github.com>
2024-10-16 20:57:37 +0900

    [Silver V] Title: 단어 정렬, Time: 68 ms, Memory: 6096 KB -BaekjoonHub

---
+++ 백준/Silver/1181. 단어 정렬/단어 정렬.cc
@@ -0,0 +1,56 @@
+#include <iostream>
+#include <cstdio>
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+// void solve(int n);
+void solve(vector<string> dict);
+
+bool compare(string temp1, string temp2);
+
+int main() {
+     int n;
+     scanf("%d", &n);
+     cin.ignore();
+
+     vector<string> dict;
+     for(int i=0; i<n; i++) {
+          string str;
+          getline(cin , str);
+
+          dict.push_back(str);
+     }
+     
+     solve(dict);
+
+     return 0;
+}
+
+void solve(vector<string> dict) {
+     sort(dict.begin(), dict.end(), compare);
+
+     for(vector<string>::iterator iter=dict.begin(); iter!=dict.end(); iter++) {
+          if(*iter!=*(iter+1)) {
+               printf("%s\n", (*iter).c_str());
+          }
+     }
+     return;
+}
+
+bool compare(string temp1, string temp2) {
+     if(temp1.length()!=temp2.length()) return temp1.length() < temp2.length();
+     else {
+          for(int i=0; i<temp1.length(); i++) {
+               if(temp1[i]!=temp2[i]) {
+                    // printf("%c %c\n", temp1[i], temp2[i]);
+
+                    return temp1[i]<temp2[i];
+               }  
+          }
+
+          // return true;
+     } 
+}
