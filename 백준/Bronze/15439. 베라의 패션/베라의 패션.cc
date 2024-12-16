9d002127e81a13cad8c6c703f579a2c74a38555a
fkhl1234 <149237563+fkhl1234@users.noreply.github.com>
2024-12-16 11:33:09 +0900

    [Bronze IV] Title: 베라의 패션, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/15439. 베라의 패션/베라의 패션.cc
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <cstdio>
+#include <string>
+#include <sstream>
+#include <vector>
+#include <algorithm>
+#include <set>
+#include <map>
+#include <stack>
+#include <queue> 
+
+using namespace std;
+
+int main() {
+     int n;
+     scanf("%d", &n);
+
+     printf("%d\n", n*(n-1));
+}
