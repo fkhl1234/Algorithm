c5bd577edf66114b5d1908eefa39dbc13ada8a46
fkhl1234 <149237563+fkhl1234@users.noreply.github.com>
2025-12-17 13:44:50 +0900

    [Silver III] Title: 퇴사, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

--- 백준/Silver/14501. 퇴사/퇴사.cc
+++ 백준/Silver/14501. 퇴사/퇴사.cc
@@ -1,57 +1,9 @@
 #include <stdio.h>
 
-void swap(int* a, int* b) {
-    int temp = *a;
-    *a = *b;
-    *b = temp;
-}
-
-int partition(int arr[], int low, int high) {
-    int pivot = arr[high];
-    int i = low - 1;
-
-    for(int j=low; j<high; j++) {
-        if(arr[j] <= pivot) {
-            i++;
-            swap(&arr[i], &arr[j]);
-        }
-    }
-
-    swap(&arr[i+1], &arr[high]);
-
-    return i+1;
-}
-
-void quickSort(int arr[], int low, int high) {
-    if(low < high) {
-        int pi = partition(arr, low, high);
-
-        quickSort(arr, low, pi-1);
-        quickSort(arr, pi+1, high);
-    }
-}
-
 int delay[16] = {0, };
 int cost[16] = {0, };
 
 int N;
-int max_cost = 0;
-
-void DFS(int start, int sum) {
-    if(start + delay[start] - 1 > N) return;
-    
-    // printf("start : %d\n", start);
-    sum += cost[start];
-
-    if(sum > max_cost) max_cost = sum;
-
-    for(int i=start + delay[start]; i<=N; i++) {
-        DFS(i, sum);
-    }
-
-    return;
-}
-
 
 int main() {
     scanf("%d", &N);
@@ -60,11 +12,17 @@ int main() {
         scanf("%d %d", &delay[i], &cost[i]);
     }
 
-    for(int i=1; i<=N; i++) {
-        DFS(i, 0);
+    int dp[17] = {0, };
+
+    for(int i=N; i>=1; i--) {
+        if(i+delay[i]-1 <= N) {
+            dp[i] = (cost[i] + dp[i+delay[i]] > dp[i+1]) ?  cost[i] + dp[i+delay[i]] : dp[i+1];
+        } else {
+            dp[i] = dp[i+1];
+        }
     }
 
-    printf("%d\n", max_cost);
+    printf("%d\n", dp[1]);
 
     return 0;
 }
